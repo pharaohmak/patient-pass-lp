afc850abab40be2aecfc7ff1e383dbc914c7252f
Dane Porter <dane@porters.net>
2024-09-08 02:11:13 -0600

    last chance for tonight.
    i am tyred ..

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: ['react-app'],
+    plugins: [
+      '@babel/plugin-proposal-private-property-in-object',
+      '@babel/plugin-transform-private-property-in-object'
+    ]
+  };
+  
