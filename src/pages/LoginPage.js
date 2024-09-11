d5ba100379a9ce6adfa0e2e50e32437de31e34d5
Dane Porter <dane@porters.net>
2024-09-11 11:13:14 -0700

    Unused variables on LoginPage.js removed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+/* import { Link } from 'react-router-dom';
+*/
 import './MainPage.css'; // Referencing the existing CSS file
 
 function LoginPage() {
