f39e193bc851d50db5810d3641a0547bdc0cede8
Dane Porter <dane@porters.net>
2024-10-01 17:35:29 -0600

    Font coloring fixes

--- src/pages/ConfirmationMessage.js
+++ src/pages/ConfirmationMessage.js
@@ -7,12 +7,15 @@ function ConfirmationMessage() {
     <div className="SignUp">
       <div className="SignUp-container">
         <header className="SignUp-header">
+        <div className="confirmation-message">
           <h1>Success!</h1>
           <h3>
           Thank you for choosing PatientPass. We value your time and will be contacting you shortly. 
           </h3>
+          <br></br>
+          </div>
         </header>
-        <Link to="/" className="MainPage-SignUp-Button">
+        <Link to="/" className="LoginPage-SignIn-Button">
           Return to Home
         </Link>
       </div>
